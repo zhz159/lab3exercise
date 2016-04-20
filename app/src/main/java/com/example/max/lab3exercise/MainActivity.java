6235dab382d2d42b3d1191bc6d6af3dc9678782c
zhz159 <zhz159@ucsd.edu>
2016-04-20 01:48:37 -0700

    this doesnt work

--- app/src/main/java/com/example/max/lab3exercise/MainActivity.java
+++ app/src/main/java/com/example/max/lab3exercise/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public int sum(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public void onClick(View v) {
